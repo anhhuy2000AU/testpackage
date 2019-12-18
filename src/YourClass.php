55efd72b6bd5825d09ce419e1f0b9bc896a7cb67
alannguyen19 <alan.nguyen@brighte.com.au>
2019-12-19 10:47:04 +1100

    Initial commit

---
+++ src/YourClass.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace anhhuy2000\TestPackage;
+
+/**
+ * Testing package class
+*/
+class YourClass
+{
+    /**
+     * Testing package function
+     *
+     * @return string
+     */
+    public function sayHello()
+    {
+         return "Hello World";
+    }
+}
